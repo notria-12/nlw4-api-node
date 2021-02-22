b5db06109cdacd15ed08135127f269413ad675e5
notria-12 <airton.araujo@acad.ifma.edu.br>
2021-02-22 11:08:12 -0300

    conclusão primeira aula

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,15 @@ import  express from 'express';
 
 const app = express();
 
+
+app.get('/users', (req, res) =>{
+    return res.send("Hello world ")
+});
+
+app.post('/', (req, res) =>{
+    return res.json({message: "Os dados foram salvos com sucesso!"})
+})
+
 app.listen('3333', () =>{
     console.log('Server is running');
 })
