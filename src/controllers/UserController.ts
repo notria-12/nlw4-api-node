3698e5263b4976d596cf6db6e54428c05f85e953
Airton Araujo <airton.araujo.s18@gmail.com>
2021-02-25 08:02:40 -0300

    adicionando rotas em arquivo separado

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,14 @@
+import {  Request, Response} from "express";
+class UserController{
+
+    async create( req: Request, res: Response){
+        const body = req.body;
+
+        console.log(body);
+
+        return res.send();
+    }
+
+}
+
+export{UserController}
