7538ae3895db7c7da92a383ab833d4d3190897ad
notria-12 <airton.araujo@acad.ifma.edu.br>
2021-02-25 09:49:52 -0300

    conexão com o BD e criação de usuário

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,12 +1,37 @@
-import {  Request, Response} from "express";
+import {  Request, response, Response} from "express";
+import { getRepository } from "typeorm";
+import { User } from "../models/User";
+
 class UserController{
 
     async create( req: Request, res: Response){
-        const body = req.body;
+        const {name, email} = req.body;
 
-        console.log(body);
+        const usersRepository = getRepository(User);
+        
+        //SELECT * FROM USERS WHERE EMAIL = "EMAIL"
+        // let users = await usersRepository.find();
+        // console.log(users)
+        const userAlreadyExists = await usersRepository.findOne({
+            email,
+        });
+        console.log(userAlreadyExists);
 
-        return res.send();
+        if(userAlreadyExists ){
+            return res.status(400).json({
+                error: "User already exists"
+            })
+        }
+
+        const user = usersRepository.create(
+            {
+                name, email
+            }
+        );
+
+        await usersRepository.save(user);
+
+        return res.json(user);
     }
 
 }
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,12 @@
 import 'reflect-metadata';
 import  express from 'express';
 import './database';
+import { router } from './routes';
 
 const app = express();
 
-
-app.get('/users', (req, res) =>{
-    return res.send("Hello world ")
-});
-
-app.post('/', (req, res) =>{
-    return res.json({message: "Os dados foram salvos com sucesso!"})
-})
+app.use(express.json());
+app.use(router);
 
 app.listen('3333', () =>{
     console.log('Server is running');
